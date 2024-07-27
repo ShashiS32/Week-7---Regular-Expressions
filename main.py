0280877c52bc92e12ee47d37c6bd36d15f38dfb9
Shashwat Singh <shashwats17@gmail.com>
2024-07-27 10:47:05 -0400

    re

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What is your email?: ").strip()
 
-if re.search(r"^\w+@[^0-9]\w+\.edu$", email):
+if re.search(r"^\w+@[^0-9]\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
